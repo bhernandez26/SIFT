fa249b55dfd2edeaee1449f74facaf60debe122d
Brandon Hernandez <heihachi501@gmail.com>
2019-03-31 19:25:30 -0400

    Sift

--- sift.py
+++ sift.py
@@ -8,12 +8,8 @@ img = cv2.imread('./andreas_L-150x150.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 sift = cv2.xfeatures2d.SIFT_create()
-keypoints, descriptor = sift.detectAndCompute(gray, NONE)
+keypoints, descriptor = sift.detectAndCompute(gray, None)
 
-img = cv2.drawKeypoints(gray, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+img = cv2.drawKeypoints(gray, keypoints, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 cv2.imwrite('sift_keypoint.jpg', img)
-
-
-
-
