bc4ccad161b955df1099fb139cedf27aac5836da
Brandon Hernandez <heihachi501@gmail.com>
2019-03-31 18:50:06 -0400

    Initial Commit

---
+++ sift.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2 as cv2
+from matplotlib import pyplot as plt
+import sys
+
+
+img = cv2.imread('./andreas_L-150x150.png')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+sift = cv2.xfeatures2d.SIFT_create()
+keypoints, descriptor = sift.detectAndCompute(gray, NONE)
+
+img = cv2.drawKeypoints(gray, kp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+cv2.imwrite('sift_keypoint.jpg', img)
+
+
+
+
